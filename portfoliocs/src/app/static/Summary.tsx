4064cd3877b98a4c8b1f16840cbe3416b97dbc78
Courtney Suhr <courtneylsuhr@gmail.com>
2024-11-06 11:37:33 +0000

    updates

--- portfoliocs/src/app/static/Summary.tsx
+++ portfoliocs/src/app/static/Summary.tsx
@@ -9,20 +9,20 @@ function Summary() {
       >
         <p className="mb-5">
           From Neuroscientist to HR Leader to Software Developer, my journey
-          reflects a diverse and dynamic career path. I had the recent privilege
-          of joining a tech startup as hire number 8 and supported its growth to
-          a successful acquisition. Working with passionate technical teams
+          reflects a diverse and dynamic career path. I had the privilege of
+          joining a tech startup as hire number 8 and supported its growth to a
+          successful acquisition. Working with passionate technical teams
           sparked my interest in software development, and my recent bootcamp
           experience at Maker&apos;s Academy solidified my desire to pursue this
           path.
           <br />
-          I&apos;m seeking my first development role, confident that I&apos;ll
-          be a risk you&apos;ll never regret taking. What I lack in experience,
-          I make up for with attitude - bring on the challenges!
+          I&apos;ve recently joined Solirius Consulting as a Test Engineer,
+          dedicated to expanding my expertise in languages, tools and processes
+          that allow me to contribute effectively to quality assurance
+          workflows.
           <br />
-          In the meantime, I&apos;m not one to sit still. I&apos;m proactively
-          engaging and working with clients to revamp their websites with modern
-          web applications.{" "}
+          In my spare time I continue to work on projects that spark my interest
+          (see below), with a new movie recommendation web app in the works.{" "}
         </p>
       </article>
     </>
